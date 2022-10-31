a0e126056c7731d1a774546cd87ccf8d8a9f31c0
Vyacheslav Tumanov <me@slavatumanov.me>
2022-10-31 14:49:43 +0500

    Prevent double applying annotation when namespace annotation is used
    Signed-off-by: Vyacheslav Tumanov <me@slavatumanov.me>

--- main.go
+++ main.go
@@ -86,7 +86,12 @@ func (s *admissionWebhookServer) Review(ctx context.Context, in *admissionv1.Adm
 	annotation := podMetaPtr.Annotations[s.config.Annotation]
 
 	// use namespace annotation only if resource doesn't have it's own
-	if annotation == "" {
+	if annotation == "" && in.Kind.Kind != "Pod" {
+		resp.Allowed = true
+		return resp
+	}
+
+	if annotation == "" && in.Kind.Kind == "Pod" {
 		timeoutCtx, cancel := context.WithTimeout(ctx, time.Second)
 		defer cancel()
 		namespace, err := s.clientset.CoreV1().Namespaces().Get(timeoutCtx, in.Namespace, v1.GetOptions{})
